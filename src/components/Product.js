e4b2c1d06e2669bbf48ebc3358a96d752dd05c89
karthikshanbhag <karthikshanbhag969@gmail.com>
2022-10-04 13:42:42 +0530

    adding

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 export default function Product({products}) {
+  console.log(products.id)
   return (
     <div className="col-md-3 m-5 card p-2">
     <div >
--- src/screens/Productdescscreen.js
+++ src/screens/Productdescscreen.js
@@ -2,8 +2,10 @@ import React from 'react'
 import products from '../products'
 import { useParams } from 'react-router-dom'
 export default function Productdescscreen() {
-    const {ide} = useParams()
-    const disp = products.find(disp=>disp.id===ide)
+    const {ide} = useParams();
+    const disp = products.find(obj=>{
+      return obj.id===2
+    })
   return (
     <div>
         <h1>ujihsdgfiogqwadof</h1>
