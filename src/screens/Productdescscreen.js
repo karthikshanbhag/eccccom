7b4e7720286fc9b53299de889be41a1aea3a5571
karthikshanbhag <karthikshanbhag969@gmail.com>
2022-10-04 14:08:10 +0530

    third

--- src/screens/Productdescscreen.js
+++ src/screens/Productdescscreen.js
@@ -7,7 +7,7 @@ export default function Productdescscreen({match}) {
   return (
     <div>
         <h1>ujihsdgfiogqwadof</h1>
-        <h1>{disp.name}</h1>
+        <h1>{product.name}</h1>
        {/* <h1>{ide}</h1>  */}
     </div>
   )
